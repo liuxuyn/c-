0ac69fdff410a0093b20d663555f165b2eea9c4f
liuxuyn <66624617+liuxuyn@users.noreply.github.com>
2020-08-06 09:42:56 +0800

    Delete dialog.h

--- dialog.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIALOG_H_INCLUDED
-#define DIALOG_H_INCLUDED
-
-#include "common.h"
-#include "draw.h"
-#include "key.h"
-extern int MouseX,MouseY,press;
-void exitmenu(imagesave *storage);
-#endif // DIALOG_H_INCLUDED
